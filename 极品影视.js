51d51093ff87441fc25c1e089ab8580d9ed0a88b
zzzlllmmm1 <82128875+zzzlllmmm1@users.noreply.github.com>
2026-06-17 14:33:01 +0800

    22

---
+++ 极品影视.js
@@ -0,0 +1,28 @@
+var rule = {
+  title: '极品影视',
+  host: 'https://tvjp.cc/',
+  url: '/vodshow/fyclass--------fypage---.html',
+  searchUrl: '/vodsearch/**----------fypage---.html',
+  searchable: 2,
+  quickSearch: 0,
+  filterable: 0,
+  headers: {
+    'User-Agent': 'UC_UA',
+  },
+  class_parse: '.stui-header__menu li:gt(0):lt(7);a&&Text;a&&href;.*/(.*?).html',
+  play_parse: true,
+  lazy: "js:\n  let html = request(input);\n  let hconf = html.match(/r player_.*?=(.*?)</)[1];\n  let json = JSON5.parse(hconf);\n  let url = json.url;\n  if (json.encrypt == '1') {\n    url = unescape(url);\n  } else if (json.encrypt == '2') {\n    url = unescape(base64Decode(url));\n  }\n  if (/\\.(m3u8|mp4|m4a|mp3)/.test(url)) {\n    input = {\n      parse: 0,\n      jx: 0,\n      url: url,\n    };\n  } else {\n    input;\n  }",
+  limit: 6,
+  double: true,
+  推荐: 'ul.stui-vodlist.clearfix;li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
+  一级: '.stui-vodlist li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
+  二级: {
+    title: '.stui-content__detail .title&&Text;.stui-content__detail&&p:eq(3)&&a&&Text',
+    img: '.stui-content__thumb .lazyload&&data-original',
+    desc: '.stui-content__detail p&&Text;.stui-content__detail&&p:eq(2)&&Text;.stui-content__detail&&p:eq(4)&&Text;.stui-content__detail p:eq(7)&&Text;.stui-content__detail p:eq(8)&&Text',
+    content: '.stui-content__detail p:eq(-1)&&Text',
+    tabs: '.stui-vodlist__head h4',
+    lists: '.stui-content__playlist:eq(#id) li',
+  },
+  搜索: 'ul.stui-vodlist__media,ul.stui-vodlist,#searchList li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href;.detail&&Text',
+}
